7c5247306dee0608cede077c1c3acc4fc0c18d30
Shiroe <aggelosteck@gmail.com>
2025-06-30 17:23:21 +0300

    updated next config to build svgr properly
    - turbopack is only used in dev mode so by default the build stil uses
    webpack, hence both are required in the next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,17 @@ const nextConfig: NextConfig = {
   turbopack: {
     rules: {
       "*.svg": {
-        loaders: ["@svgr/webpack"],
         as: "*.js",
+        loaders: ["@svgr/webpack"],
       },
     },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
+    return config;
   },
 };
 
